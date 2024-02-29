b2a9580bc97e37430461e523f23691318cb18434
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 10:05:08 +0100

    Dzień 1: Krok 2: Uruchomienie aplikacji - W metodzie Main tworzymy instancję klasy TicTacToe

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TicTacToe;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TicTacToe game = new TicTacToe();
+        game.PlayGame();
+        Console.ReadLine(); // Tutaj czekamy, aż gracz przeczyta wynik gry, zanim zamknie konsolę.
+    }
+}
