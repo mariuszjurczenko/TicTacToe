1fb2fd53ab285afe633f10e38e92a21e678c0e10
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 18:33:53 +0100

    Dzień 3. Krok 8. Konfiguracja kontenera IoC - "Wielka premiera"

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,4 +1,9 @@
 ﻿using TicTacToe.Gameplay;
+using TicTacToe.Interfaces;
+using TicTacToe.IoC;
+using TicTacToe.Models;
+using TicTacToe.Services;
+using TicTacToe.UI;
 
 namespace TicTacToe;
 
@@ -6,7 +11,20 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var game = new Game(); // Tutaj przywołujemy naszych bohaterów - kółko i krzyżyk.
-        game.PlayGame(); // I tak, zaczyna się nasza epicka rozgrywka.
+        var container = new SimpleContainer();
+        var gameUIWrapper = new GameUIWrapper();
+
+        // Rejestracja typów
+        container.For<IPlayerFactory>().Use<PlayerFactory>();
+        container.For<IBoard>().Use<Board>();
+        container.For<IGameEngine>().Use<GameEngine>();
+        container.ForSingleton<IGameUI>(gameUIWrapper);
+
+        // Rozwiązanie zależności
+        var gameEngine = container.Resolve<IGameEngine>();
+        var gameUI = container.Resolve<IGameUI>();
+
+        var game = new Game(gameEngine, gameUI);
+        game.PlayGame();
     }
 }
