14c48f25c5599226c557aa95f194f32f3b095878
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 17:03:26 +0100

    Dzień 2. Krok 2. Definicja klasy Player - "Wejście bohaterów"

---
+++ TicTacToe/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace TicTacToe;
+
+public class Player
+{
+    public char Symbol { get; private set; }    // To ich znak rozpoznawczy, jak logo Batmana na niebie nad Gotham.
+
+    public Player(char symbol)
+    {
+        Symbol = symbol;    // Przydzielamy każdemu graczowi jego unikatowy symbol, jak wybierając superbohaterowi jego kostium.
+    }
+
+    public override string ToString()
+    {
+        // Tutaj gracze mówią światu, kim są - za pomocą swojego symbolu.
+        return string.Format(GameTexts.PlayerRepresentation, Symbol);   // Każdy gracz jest gotów wskoczyć na planszę i stawić czoła wyzwaniu.
+    }
+}
