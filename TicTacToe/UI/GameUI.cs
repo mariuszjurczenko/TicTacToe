80a506f463c90c8091de6bb07e7c19055cb7cad1
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 19:44:26 +0100

    Dzień 5. 2. Krzyżówka Wyborów Krok 3. Zaklęcia w GameUI - Modyfikacja GameUI

--- TicTacToe/UI/GameUI.cs
+++ TicTacToe/UI/GameUI.cs
@@ -34,4 +34,14 @@ public static class GameUI
         string answer = Console.ReadLine().ToLower();
         return answer == GameTexts.ConfirmYes;
     }
+
+    public static bool AskForGameMode()
+    {
+        // Ah, wielki wybór przed Tobą, o wędrowcze cyfrowych krain! Czy zechcesz zmierzyć się z bezduszną maszyną
+        // czy raczej wybierzesz ciepło ludzkiego towarzystwa? Wybierz mądrze, gdyż od tego zależy Twoja dalsza ścieżka.
+        Console.WriteLine(GameTexts.ChooseGameMode);
+        var choice = Console.ReadLine();
+        // 1 dla AI, cokolwiek innego i masz szansę na ludzki kontakt. Chyba że to też bot. W takim razie, powodzenia!
+        return choice == "1";
+    }
 }
