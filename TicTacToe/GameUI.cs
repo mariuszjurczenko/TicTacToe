0cb25716b99b95bfb9d4447a1c88650eed5d1f99
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 17:09:06 +0100

    Dzień 2. Krok 5. Definicja klasy GameUI - "Mistrz Ceremonii Naszej Gry"

---
+++ TicTacToe/GameUI.cs
@@ -0,0 +1,35 @@
+﻿namespace TicTacToe;
+
+public static class GameUI
+{
+    public static int GetInput(string prompt, int min, int max)
+    {
+        int input = -1;
+        bool isValidInput = false;
+
+        while (!isValidInput)
+        {
+            Console.Write(prompt);
+            isValidInput = int.TryParse(Console.ReadLine(), out input) && input >= min && input <= max;
+
+            if (!isValidInput)
+            {
+                Console.WriteLine(string.Format(GameTexts.ErrorInputRange, min, max));
+            }
+        }
+
+        return input;
+    }
+
+    public static void PrintMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static bool AskToPlayAgain()
+    {
+        Console.WriteLine(GameTexts.PromptPlayAgain);
+        string answer = Console.ReadLine().ToLower();
+        return answer == GameTexts.ConfirmYes;
+    }
+}
