8c2cd33d150b0b35f7f1ea923212f5099ffadbcc
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 17:07:52 +0100

    Dzień 2. Krok 4. Definicja klasy GameEngine - "Mózg Operacji"

---
+++ TicTacToe/GameEngine.cs
@@ -0,0 +1,93 @@
+﻿namespace TicTacToe;
+
+public class GameEngine
+{
+    private Player playerX;
+    private Player playerO;
+
+    public Player CurrentPlayer { get; private set; }
+    public Board Board { get; private set; }
+    public bool IsGameRunning { get; private set; }
+
+    public GameEngine()
+    {
+        Board = new Board();
+        playerX = new Player(GameTexts.SymbolX);
+        playerO = new Player(GameTexts.SymbolO);
+        CurrentPlayer = playerX;    // X zawsze zaczyna, bo tradycja tak nakazuje
+        IsGameRunning = true;   // Jak w dobrym serialu, gra trwa, dopóki widzowie (gracze) nie zdecydują inaczej
+    }
+
+    public void SwitchPlayer()
+    {
+        CurrentPlayer = CurrentPlayer == playerX ? playerO : playerX; // Zmiana graczy jest jak zmiana kierunków wiatru
+    }
+
+    public bool CheckForWinner()
+    {
+        char[,] myBoard = Board.GetBoard();
+        char player = CurrentPlayer.Symbol;
+
+        // Tu sprawdzamy wiersze, kolumny i przekątne jak detektyw szukający wskazówek
+        for (int i = 0; i < 3; i++)
+        {
+            // Sprawdzanie wierszy i kolumn
+            if (myBoard[i, 0] == player && myBoard[i, 1] == player && myBoard[i, 2] == player ||
+                myBoard[0, i] == player && myBoard[1, i] == player && myBoard[2, i] == player)
+                return true;    // Mamy zwycięzcę!
+        }
+
+        // Sprawdzanie przekątnych
+        if (myBoard[0, 0] == player && myBoard[1, 1] == player && myBoard[2, 2] == player ||
+            myBoard[0, 2] == player && myBoard[1, 1] == player && myBoard[2, 0] == player)
+        {
+            return true;    // Zwycięstwo na przekątnej, jak złoto olimpijskie w skokach narciarskich!
+        }
+
+        return false;   // Gra toczy się dalej
+    }
+
+    public bool CheckForTie()
+    {
+        // Sprawdzamy, czy na planszy są jeszcze wolne miejsca
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (Board.IsFieldEmpty(i, j))
+                    return false;   // Jeszcze jest miejsce, gra trwa.
+            }
+        }
+
+        // Jeśli nie ma wolnych miejsc i nikt nie wygrał, to mamy remis.
+        return !CheckForWinner();
+    }
+
+    public void InitializeBoard()
+    {
+        Board.InitializeBoard();    // Reset planszy jak po naciśnięciu magicznego przycisku.
+    }
+
+    public void MakeMove(int row, int column)
+    {
+        if (Board.IsFieldEmpty(row, column))
+        {
+            Board.SetMove(row, column, CurrentPlayer.Symbol);   // Ruch gracza to jak zaznaczenie terytorium.
+        }
+    }
+
+    public void SetGameRunning(bool isRunning)
+    {
+        IsGameRunning = isRunning;  // Kontrolujemy, czy gra ma się toczyć dalej.
+    }
+
+    public void PrintBoard()
+    {
+        Board.PrintBoard(); // Wyświetlamy planszę jak najnowsze dzieło w galerii sztuki.
+    }
+
+    public bool IsFieldEmpty(int row, int column)
+    {
+        return Board.IsFieldEmpty(row, column); // Sprawdzamy, czy pole jest wolne, jak parking w niedzielę rano.
+    }
+}
