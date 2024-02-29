ea62ea4d19a63cbc987fb686a47aa7679087734d
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 18:26:14 +0100

    Dzień 3. Krok 5. Definiowanie fabryki graczy - "Kuźnia legendarnych wojowników"

---
+++ TicTacToe/Services/PlayerFactory.cs
@@ -0,0 +1,12 @@
+﻿using TicTacToe.Interfaces;
+using TicTacToe.Models;
+
+namespace TicTacToe.Services;
+
+public class PlayerFactory : IPlayerFactory
+{
+    public IPlayer CreatePlayer(char symbol)
+    {
+        return new Player(symbol);
+    }
+}
