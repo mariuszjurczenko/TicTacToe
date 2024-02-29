a15e0a6023a80650ea6d03af0569a12f6c3c5851
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 18:32:49 +0100

    Dzień 3. Krok 7. Dodajemy nasz prosty kontenera IoC - "Przywołanie Czarodzieja do Zarządzania Magią Zależności"

---
+++ TicTacToe/IoC/SimpleContainer.cs
@@ -0,0 +1,91 @@
+﻿namespace TicTacToe.IoC;
+
+public class SimpleContainer
+{
+    Dictionary<Type, Type> _map = new Dictionary<Type, Type>();
+    Dictionary<Type, object> _singletons = new Dictionary<Type, object>();
+
+    public ContainerBuilder For<TSource>()
+    {
+        return For(typeof(TSource));
+    }
+
+    public ContainerBuilder For(Type sourceType)
+    {
+        return new ContainerBuilder(this, sourceType);
+    }
+
+    public ContainerBuilder ForSingleton<TSource>(TSource instance)
+    {
+        _singletons[typeof(TSource)] = instance;
+        return For<TSource>();
+    }
+
+    public TSource Resolve<TSource>()
+    {
+        return (TSource)Resolve(typeof(TSource));
+    }
+
+    public object Resolve(Type sourceType)
+    {
+        if (_singletons.ContainsKey(sourceType))
+        {
+            return _singletons[sourceType];
+        }
+        else if (_map.ContainsKey(sourceType))
+        {
+            var destinationType = _map[sourceType];
+            return CreateInstance(destinationType);
+        }
+        else if (sourceType.IsGenericType && _map.ContainsKey(sourceType.GetGenericTypeDefinition()))
+        {
+            var destination = _map[sourceType.GetGenericTypeDefinition()];
+            var closedDestination = destination.MakeGenericType(sourceType.GenericTypeArguments);
+
+            return CreateInstance(closedDestination);
+        }
+        else if (!sourceType.IsAbstract)
+        {
+            return CreateInstance(sourceType);
+        }
+        else
+        {
+            throw new InvalidOperationException("Could not resolve " + sourceType.FullName);
+        }
+    }
+
+    private object CreateInstance(Type destinationType)
+    {
+        var paramters = destinationType.GetConstructors()
+                            .OrderByDescending(c => c.GetParameters().Count())
+                            .First()
+                            .GetParameters()
+                            .Select(p => Resolve(p.ParameterType))
+                            .ToArray();
+
+        return Activator.CreateInstance(destinationType, paramters);
+    }
+
+    public class ContainerBuilder
+    {
+        SimpleContainer _container;
+        Type _sourceType;
+
+        public ContainerBuilder(SimpleContainer container, Type sourceType)
+        {
+            _container = container;
+            _sourceType = sourceType;
+        }
+
+        public ContainerBuilder Use<TDestination>()
+        {
+            return Use(typeof(TDestination));
+        }
+
+        public ContainerBuilder Use(Type destinationType)
+        {
+            _container._map.Add(_sourceType, destinationType);
+            return this;
+        }
+    }
+}
