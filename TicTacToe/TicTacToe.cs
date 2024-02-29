3f08c36806dfa433c5c9f0b6abfe092249014242
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 10:04:15 +0100

    Dzień 1: Krok 1: Zaczynamy - Definicja klasy TicTacToe

---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToe;
+
+public class TicTacToe
+{
+    // Tutaj dodamy nasze zmienne i metody
+    public void PlayGame()
+    {
+        // Tutaj rozpocznie się magia gry
+    }
+}
