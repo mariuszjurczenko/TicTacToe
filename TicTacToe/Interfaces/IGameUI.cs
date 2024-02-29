f9ee065853895b1f3c8f94ea47f001f169ac6b9a
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-02-29 19:46:14 +0100

    Dzień 5. 2. Krzyżówka Wyborów Krok 5. Przebudzenie IGameUI - Modyfikacja IGameUI

--- TicTacToe/Interfaces/IGameUI.cs
+++ TicTacToe/Interfaces/IGameUI.cs
@@ -5,4 +5,8 @@ public interface IGameUI
     int GetInput(string prompt, int min, int max);
     void PrintMessage(string message);
     bool AskToPlayAgain();
+    // O, wielka poro, która stoi przed każdym graczem, nim jeszcze pierwszy ruch zostanie wykonany.
+    // Czy zdecydujesz się na samotną podróż w starciu z niezgłębioną logiką AI,
+    // czy też wybierzesz bardziej ludzką ścieżkę, stając oko w oko z innym śmiałkiem?
+    bool AskForGameMode();
 }
