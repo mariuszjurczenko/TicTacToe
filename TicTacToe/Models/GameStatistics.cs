9ad2fd2d87506f105ce79df11ada1c2434266e83
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-03-01 08:46:28 +0100

    Dzień 6. 2 Dodanie statystyk gry Krok 2. Dodanie Klasy Dla Statystyk - "Księga rekordów: Gdzie każda liczba ma swoją opowieść"

---
+++ TicTacToe/Models/GameStatistics.cs
@@ -0,0 +1,22 @@
+﻿using TicTacToe.Interfaces;
+using TicTacToe.Utils;
+
+namespace TicTacToe.Models;
+
+public class GameStatistics : IGameStatistics
+{
+    public int WinsX { get; private set; }
+    public int WinsO { get; private set; }
+    public int Ties { get; private set; }
+
+    public void IncrementWinsX() => WinsX++;
+
+    public void IncrementWinsO() => WinsO++;
+
+    public void IncrementTies() => Ties++;
+
+    public override string ToString()
+    {
+        return string.Format(GameTexts.Statistics, WinsX, WinsO, Ties);
+    }
+}
