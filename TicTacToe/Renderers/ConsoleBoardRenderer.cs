c368ff91368e3272588dd839044b70850e6189f1
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-03-01 09:33:31 +0100

    Dzień 7. 2 Poprawa wizualizacji planszy w grze Krok 5. Poprawa wizualizacji planszy w grze

--- TicTacToe/Renderers/ConsoleBoardRenderer.cs
+++ TicTacToe/Renderers/ConsoleBoardRenderer.cs
@@ -7,13 +7,60 @@ public class ConsoleBoardRenderer : IBoardRenderer
 {
     public void RenderBoard(Board board)
     {
-        for (int i = 0; i < board.GetSize(); i++)
+        int boardRows = board.GetBoard().GetLength(0);
+        int boardCols = board.GetBoard().GetLength(1);
+
+        // Szerokość i wysokość komórki wizualnej, łącznie z obramowaniem
+        int cellWidth = 4;
+        int cellHeight = 2;
+
+        // Całkowita szerokość i wysokość planszy wizualnej
+        int boardWidth = (boardCols * cellWidth) + 1;   // +1 dla linii po prawej stronie
+        int boardHeight = (boardRows * cellHeight) + 1; // +1 dla linii na dole
+
+        // Obliczanie marginesów, aby wyśrodkować planszę
+        int consoleWidth = Console.WindowWidth;
+        int consoleHeight = Console.WindowHeight;
+
+        int leftPadding = (consoleWidth - boardWidth) / 2;
+        int topPadding = (consoleHeight - boardHeight) / 2;
+
+        // Czyszczenie konsoli i ustawienie kursora
+        Console.Clear();
+
+        // Rysowanie szachownicy
+        for (int row = 0; row <= boardRows; ++row)
         {
-            for (int j = 0; j < board.GetSize(); j++)
+            // Rysowanie górnej linii każdego rzędu komórek
+            Console.SetCursorPosition(leftPadding, topPadding + (row * cellHeight));
+            Console.Write("+");
+            for (int col = 0; col < boardCols; ++col)
             {
-                Console.Write(board.GetSymbol(i, j));
+                Console.Write(new string('-', cellWidth - 1) + "+");
             }
-            Console.WriteLine();
+
+            // Przejście do kolejnych linii komórek, jeśli nie jest to ostatni rząd
+            if (row < boardRows)
+            {
+                for (int line = 1; line <= cellHeight; ++line)
+                {
+                    Console.SetCursorPosition(leftPadding, topPadding + (row * cellHeight) + line);
+                    for (int col = 0; col <= boardCols; ++col)
+                    {
+                        // Rysowanie linii pionowych komórek i symbole wewnątrz
+                        Console.Write("|");
+                        if (col < boardCols)
+                        {
+                            Console.ForegroundColor = (board.GetSymbol(row, col) == 'X') ? ConsoleColor.Green : (board.GetSymbol(row, col) == 'O') ? ConsoleColor.Red : ConsoleColor.White;
+                            Console.Write(" " + (board.GetSymbol(row, col) != '\0' ? board.GetSymbol(row, col).ToString() : " ") + " ");
+                            Console.ResetColor();
+                        }
+                    }
+                }
+            }
         }
+
+        // Ustawienie kursora na koniec, aby nie nadpisywać planszy
+        Console.SetCursorPosition(0, topPadding + boardHeight);
     }
 }
