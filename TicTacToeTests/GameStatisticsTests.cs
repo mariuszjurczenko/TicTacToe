a18c6aefe3b7387d1ef4edac116d1afbc8e55c1c
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-03-01 09:02:37 +0100

    Dzień 6. 2 Dodanie statystyk gry Krok 9. Testy jednostkowe dla klasy GameStatistics

---
+++ TicTacToeTests/GameStatisticsTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using TicTacToe.Models;
+
+namespace TicTacToe.Tests;
+
+public class GameStatisticsTests
+{
+    [Fact]
+    public void IncrementWinsX_ShouldIncrementWinsXCount_BecauseXAlwaysMarksTheSpot()
+    {
+        // Arrange
+        var stats = new GameStatistics();
+
+        // Act
+        stats.IncrementWinsX();
+
+        // Assert
+        stats.WinsX.Should().Be(1, because: "X nie tylko wyznacza miejsce, ale także oznacza nasze pierwsze zwycięstwo!");
+    }
+
+    [Fact]
+    public void IncrementWinsO_ShouldIncrementWinsOCount_BecauseOIsNotJustAZero()
+    {
+        // Arrange
+        var stats = new GameStatistics();
+
+        // Act
+        stats.IncrementWinsO();
+
+        // Assert
+        stats.WinsO.Should().Be(1, because: "O to nie tylko zera; w naszej książce są to całkowite zwycięstwa!");
+    }
+
+    [Fact]
+    public void IncrementTies_ShouldIncrementTiesCount_BecauseEvenStalematesNeedLove()
+    {
+        // Arrange
+        var stats = new GameStatistics();
+
+        // Act
+        stats.IncrementTies();
+
+        // Assert
+        stats.Ties.Should().Be(1, because: "Każdy remis przypomina, że nawet remis wymaga przytulenia.");
+    }
+
+    [Fact]
+    public void ToString_ShouldReturnCorrectFormat_BecauseStatisticsTellTheTaleOfGlory()
+    {
+        // Arrange
+        var magicNumber = 1; // The number of times we all tried to be heroes.
+        var stats = new GameStatistics();
+        stats.IncrementWinsX();
+        stats.IncrementWinsO();
+        stats.IncrementTies();
+        var expectedString = $"Wygrane X: {magicNumber}, Wygrane O: {magicNumber}, Remisy: {magicNumber}";
+
+        // Act
+        var result = stats.ToString();
+
+        // Assert
+        result.Should().Be(expectedString, because: "Każda liczba w naszych statystykach opowiada historię chwały, rozpaczy i czasami remisu.");
+    }
+}
