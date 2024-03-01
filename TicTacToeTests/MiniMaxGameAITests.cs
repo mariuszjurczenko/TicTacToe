8a0f5f6319b74c30b8e542f987ff3fe6bd46ba27
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-03-01 09:16:59 +0100

    Dzień 7. 1 Ulepszenie AI Krok 4. Dodanie testów jednostkowych - "Weryfikacja geniuszu: Sprawdzanie strategii"

---
+++ TicTacToeTests/MiniMaxGameAITests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using TicTacToe.AI;
+using TicTacToe.Models;
+
+namespace TicTacToe.Tests;
+
+public class MiniMaxGameAITests
+{
+    [Fact]
+    public void DecideMove_Should_SelectWinningMove_When_Possible()
+    {
+        // Arrange
+        var board = new Board();        // Ustawiamy planszę w stanie, gdzie AI ma natychmiastową możliwość wygranej
+        board.SetMove(0, 0, 'X');
+        board.SetMove(0, 1, 'X');
+        board.SetMove(1, 1, 'O');
+        board.SetMove(2, 0, 'O');
+        var ai = new MiniMaxGameAI();
+
+        // Act
+        var move = ai.DecideMove(board, 'X', 'O');
+
+        // Assert
+        move.Should().Be((0, 2), "AI powinno wybrać ruch wygrywający (0,2)");
+    }
+
+    [Fact]
+    public void DecideMove_Should_BlockOpponentWinningMove_When_Possible()
+    {
+        // Arrange
+        var board = new Board();        // Ustawienie tak aby gracz 'O', był o jeden ruch od wygranej
+        board.SetMove(0, 0, 'O');
+        board.SetMove(0, 2, 'O');
+        board.SetMove(1, 0, 'X');
+        board.SetMove(2, 0, 'X');
+        var ai = new MiniMaxGameAI();
+
+        // Act
+        var move = ai.DecideMove(board, 'X', 'O');
+
+        // Assert
+        move.Should().Be((0, 1), "AI powinno wybrać ruch blokujący wygraną przeciwnika (0,1)");
+    }
+}
