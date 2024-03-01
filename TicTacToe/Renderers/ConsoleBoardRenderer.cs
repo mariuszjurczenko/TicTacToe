159c8d19822df951cb66e81272170bd61507d702
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-03-01 09:25:23 +0100

    Dzień 7. 2 Poprawa wizualizacji planszy w grze Krok 2. Implementacja ConsoleBoardRenderer - "Malowanie światłem: Ożywienie planszy za pomocą kodu"

---
+++ TicTacToe/Renderers/ConsoleBoardRenderer.cs
@@ -0,0 +1,19 @@
+﻿using TicTacToe.Interfaces;
+using TicTacToe.Models;
+
+namespace TicTacToe.Renderers;
+
+public class ConsoleBoardRenderer : IBoardRenderer
+{
+    public void RenderBoard(Board board)
+    {
+        for (int i = 0; i < board.GetSize(); i++)
+        {
+            for (int j = 0; j < board.GetSize(); j++)
+            {
+                Console.Write(board.GetSymbol(i, j));
+            }
+            Console.WriteLine();
+        }
+    }
+}
