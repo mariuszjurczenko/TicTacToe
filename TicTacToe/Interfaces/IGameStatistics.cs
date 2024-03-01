beba7ae00e39815fbd845ec114f22aca92030b26
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-03-01 08:45:24 +0100

    Dzień 6. 2 Dodanie statystyk gry Krok 1. Definiujemy Interfejs - "Ramy, które ujarzmiają chaos: Struktura w służbie historii"

---
+++ TicTacToe/Interfaces/IGameStatistics.cs
@@ -0,0 +1,12 @@
+﻿namespace TicTacToe.Interfaces;
+
+public interface IGameStatistics
+{
+    int WinsX { get; }
+    int WinsO { get; }
+    int Ties { get; }
+
+    void IncrementWinsX();
+    void IncrementWinsO();
+    void IncrementTies();
+}
