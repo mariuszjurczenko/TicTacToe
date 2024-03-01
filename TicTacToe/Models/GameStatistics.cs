c99f7921cfec984a4aa136434f0bed56c45b45b9
Mariusz Jurczenko <mariuszjurczenko@gmail.com>
2024-03-01 09:04:22 +0100

    Dzień 6.  3 Dodanie zapisu/odczytu statystyk Krok 1. Rozszerzenie Klasy GameStatistics - "Zapisanie historii: Gdzie cyfry stają się opowieściami"

--- TicTacToe/Models/GameStatistics.cs
+++ TicTacToe/Models/GameStatistics.cs
@@ -19,4 +19,45 @@ public class GameStatistics : IGameStatistics
     {
         return string.Format(GameTexts.Statistics, WinsX, WinsO, Ties);
     }
+
+    public void SaveStats(string filePath)
+    {
+        try
+        {
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                sw.WriteLine(WinsX);
+                sw.WriteLine(WinsO);
+                sw.WriteLine(Ties);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Nie można zapisać statystyk: " + e.Message);
+        }
+    }
+
+    public void LoadStats(string filePath)
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                WinsX = int.Parse(sr.ReadLine());
+                WinsO = int.Parse(sr.ReadLine());
+                Ties = int.Parse(sr.ReadLine());
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            // Plik nie istnieje, zaczynamy bez wczytywania
+            WinsX = 0;
+            WinsO = 0;
+            Ties = 0;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Nie można wczytać statystyk: " + e.Message);
+        }
+    }
 }
